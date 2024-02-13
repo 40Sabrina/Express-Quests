63c1c92cea6c9d8adb2d98f7b5d989343f089ac7
40Sabrina <sabrinaaaaa@hotmail.com>
2024-02-13 14:57:23 +0100

    Challenge quete Express03 ok

--- src/app.js
+++ src/app.js
@@ -9,11 +9,13 @@ const movieControllers = require("./controllers/movieControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", movieControllers.postMovie);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 
 const users = require("./controllers/users");
 
 app.get("/api/users", users.getUsers);
 app.get("/api/users/:id", users.getUsersById);
 app.post("/api/users", users.postUser);
+app.put("/api/users/:id", users.updateUser);
 
 module.exports = app;
